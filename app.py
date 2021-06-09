e6eb45553b369f23c3f0a47108404b405f0898dc
Rohan A R <rohanar@Rohans-MacBook-Pro.local>
2021-06-09 18:50:30 -0500

    Recent

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 from flask import Flask, render_template, request
+import os
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-	return "<h1>Hello</h1>"
+def hello():	
+	render_template('index.html')
+
+@app.route('/showFiles',methods=["POST","GET"])
+def fileNames():
+	path = os.getcwd()
+	files = os.listdir(path)
+	render_template("files.html",filenames=files)
