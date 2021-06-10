ff3c005b863bf0d88d45967db40f0bdfa5419605
Rohan A R <rohanar@Rohans-MacBook-Pro.local>
2021-06-09 19:41:21 -0500

    djfbvg

--- app.py
+++ app.py
@@ -1,12 +1,8 @@
 from flask import Flask, render_template, request
+import os
+import csv
 import pandas as pd
 import numpy as np
-import os
-
-df = pd.read_csv('names.csv')
-df1=df.replace(np.nan,"",regex=True)
-
-data = df1.values.tolist()
 
 app = Flask(__name__)
 app.secret_key="ABCDEF"
@@ -17,6 +13,10 @@ if __name__ == '__main__':
   app.run(host='127.0.0.1', port=8080, debug=True)
   app.config['JSON_SORT_KEYS']=False
 
+df=pd.read_csv('names.csv')
+df1=df.replace(np.nan, '', regex=True)
+data=df1.values.tolist()
+
 @app.route('/')
 def hello():
 	return render_template('index.html')
@@ -26,4 +26,29 @@ def show_names():
     path=os.getcwd()
     path=path+"/static"
     files=os.listdir(path)
-	return render_template('names.html',fn=files)
+    return render_template('filenames.html',fn=files)
+
+@app.route('/room', methods=["POST","GET"])
+def show_details():
+    df=pd.read_csv('names.csv')
+    print(df)
+    df1=df.replace(np.nan, '', regex=True)
+    data=df1.values.tolist()
+    print(data)
+    room=request.form.get("roomnumber")
+    room2=int(room)
+
+    people=[]
+    count=0
+    for i in data:
+        if(i[2]!="" and i[2]!=" "):
+            rn=float(i[2])
+            rn1=int(i[2])
+            if(rn1==room2):
+                print(count)
+                count=count+1
+                people.append(i)
+    
+    return render_template('roomdetails.html',list1=people)
+
+
