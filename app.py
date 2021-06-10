de40346f7bb934c5ac5a613fbfe369521cf16aed
Rohan A R <rohanar@Rohans-MacBook-Pro.local>
2021-06-09 19:03:01 -0500

    Recent

--- app.py
+++ app.py
@@ -1,13 +1,22 @@
 from flask import Flask, render_template, request
 import os
+
 app = Flask(__name__)
+app.secret_key="ABCDEF"
+
+
+if __name__ == '__main__':
+    
+  app.run(host='127.0.0.1', port=8080, debug=True)
+  app.config['JSON_SORT_KEYS']=False
 
 @app.route('/')
-def hello():	
-	return "<h1>Hello</h1>"
+def hello():
+	return render_template('index.html')
 
-@app.route('/showFiles',methods=["POST","GET"])
-def fileNames():
-	path = os.getcwd()
-	files = os.listdir(path)
-	return render_template('files.html',filenames=files)
+@app.route('/names', methods=["POST","GET"])
+def show_names():
+    path=os.getcwd()
+    path=path+"/static"
+    files=os.listdir(path)
+    return render_template('index.html',fn=files)
